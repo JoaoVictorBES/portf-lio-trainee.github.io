0c898eab1f12515f9adc670e132d35e03fd20aa5
Joao Victor <victorsrodrigues99@gmail.com>
2025-01-14 20:50:41 -0300

    :construction: Estruturando header + resposividade

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+/* Function activate menu Hamburguer */
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburgerMenu.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
+});
