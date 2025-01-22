c0c4f844043e88ea30343116c8904367099a9bfd
Joao Victor <victorsrodrigues99@gmail.com>
2025-01-22 11:29:59 -0300

    Ajustando resposividade inicio e projetos em telas 768px

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,11 +10,6 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-AOS.init({
-    duration: 1000, // Duração da animação (em milissegundos)
-    once: true,
-    startEvent: 'DOMContentLoaded', // Inicia as animações após o DOM estar carregado
-    offset: 200 // Garante que a animação acontece apenas uma vez
-  });
+
 
 
